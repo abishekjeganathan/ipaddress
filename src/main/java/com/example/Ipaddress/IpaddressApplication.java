ae743fc44bfcc28938edd6e5f94a4a67b1670137
abishekabd90@gmail.com <abishekabd90@gmail.com>
2025-09-16 20:50:41 +0530

    first commit

---
+++ src/main/java/com/example/Ipaddress/IpaddressApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ipaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpaddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpaddressApplication.class, args);
+	}
+
+}
